9aeb1fb8ee4763b2a5a140ece670e1f04b8e74b5
Dmchez33 <iddembele206@gmail.com>
2022-12-12 08:24:46 +0000

    11eme commit

--- index.js
+++ index.js
@@ -16,10 +16,7 @@
     })
 
 })()
-function alertBox() {
-    el = document.getElementById("example");
-    el.style.visibility = el.style.visibility == "visible" ? "hidden" : "visible";
-}
+
 function sendMail(){
     var params = {
         nom: document.getElementById("nom").value,
